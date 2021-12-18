01e0edcccf68bef069d6f12a95baf446313c871b
telomerica <64539817+telomerica@users.noreply.github.com>
2021-12-18 18:22:35 +0100

    Add files via upload

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ import elems as el
 import nutrients as nt
 import os
 import json
+import units
 
 class GUI:
     def __init__(self):
@@ -15,6 +16,7 @@ class GUI:
         #self.gui.iconbitmap(f'{self.dir}icon.ico')
         self.on_screen = []
         self.new_screen(0)
+        self.chosen = 0
     
     def new_screen(self,x):
         
@@ -76,7 +78,7 @@ class GUI:
         
         elif x==3: 
             k=-1
-            
+            self.wtr_amount = 0
             for i in os.listdir(f"{os.getcwd()}/saved"):
                 k+=1
                 with open(f"{os.getcwd()}/saved/{i}","r") as file:
@@ -84,9 +86,7 @@ class GUI:
                     data1 = json.loads(data)
 
                 self.define_button(name=i[:-5],data1=data1,k=k)
-                
-
-            
+                            
             self.gui.geometry(f"580x{(int(k/3)*20)+400}")
             
             self.canvas = tt.Canvas(self.gui,width = 580, height=30)
@@ -106,10 +106,37 @@ class GUI:
             self.lb1234.grid(column=0,row=int(k/3)+3)
             self.on_screen.append(self.lb1234)
 
-            self.wtr_ent = tt.Entry(borderwidth=2,width=20)
+            self.wtr_ent = tt.Entry(borderwidth=2,width=23)
             self.wtr_ent.grid(row=int(k/3)+4,column=0)
             self.on_screen.append(self.wtr_ent)
-            print("yeah")
+            
+            self.wtr_btn = tt.Button(borderwidth=2,width=20,text="Add water",bg="darkgrey",command = lambda: self.wtr_add(self.wtr_ent.get()))
+            self.wtr_btn.grid(row=int(k/3)+5,column=0)
+            self.on_screen.append(self.wtr_btn)
+
+            self.wtr_lb = tt.Listbox(borderwidth=4,height=8,width=23)
+            self.wtr_lb.grid(row=int(k/3)+6,column=0)
+            self.on_screen.append(self.wtr_lb)
+
+            self.lb12345 = tt.Label(text="No nutrient is selected",width=20)
+            self.lb12345.grid(column=1,row=int(k/3)+3)
+            self.on_screen.append(self.lb12345)
+
+            self.nt_ent = tt.Entry(borderwidth=2,width=23)
+            self.nt_ent.grid(row=int(k/3)+4,column=1)
+            self.on_screen.append(self.nt_ent)
+
+            self.nt_btn = tt.Button(borderwidth=2,width=20,text="Add nutrient",bg="darkgrey",command=lambda:self.nt_add(self.nt_ent.get()))
+            self.nt_btn.grid(row=int(k/3)+5,column=1)
+            self.on_screen.append(self.nt_btn)
+
+            self.nt_lb = tt.Listbox(borderwidth=4,height=8,width=23)
+            self.nt_lb.grid(row=int(k/3)+6,column=1)
+            self.on_screen.append(self.nt_lb)
+
+            self.lb123456 = tt.Label(text="",width=20)
+            self.lb123456.grid(row=int(k/3)+3,column=3)
+            self.on_screen.append(self.lb123456)
 
     def yeyo(self):
         for i in el.elements_ls:
@@ -141,7 +168,38 @@ class GUI:
         for i in nt.nutrients_list:
             i.btn.configure(bg="darkgrey")
         n.btn.config(bg="lightblue")
+        self.chosen = n
         self.lb123.config(text=f'\"{n.name}\" is selected')
+        self.lb12345.config(text=f'Add \"{n.name}\" (mg,g,t)')
+
+    def wtr_add(self,watertxt):
+        list_wtr = watertxt.split(" ")
+        if len(list_wtr) == 1: #if no unit is passed, default unit is "liter"
+            wtr_amount = int(list_wtr[0])
+        elif len(list_wtr)==2:
+            wtr_amount = int(list_wtr[0])
+            for m in range(len(units.wt_units)):
+                for i in units.wt_units[m]:
+                    if i==list_wtr[1]:
+                        wtr_amount = wtr_amount * float(units.wt_get[m])
+        self.wtr_amount += wtr_amount
+        self.wtr_lb.insert(tt.END,f'{wtr_amount} liters added')
+        self.lb123456.config(text=f"{self.wtr_amount} of water in solution")
+
+
+    def nt_add(self,nttext): #the amount is converted to miligrams
+        list_nt = nttext.split(" ")
+        if len(list_nt) == 1: #if no unit is passed, default unit is "grams"
+            nt_amount = int(list_nt[0])*1000 #converted to miligrams
+        elif len(list_nt) == 2:
+            nt_amount = int(list_nt[0])
+            for m in range(len(units.nt_units)):
+                for i in units.nt_get[m]:
+                    if i == list_nt[1]:
+                        print(float(units.nt_get[m]))
+
+                
+
 
 the_gui = GUI()
 the_gui.gui.mainloop()
---
+++ units.py
@@ -0,0 +1,4 @@
+wt_units = [["l", "ls","liter", "liters","kg","kgs","kilo","kilos","kilograms"],["ml", "mls","mililiter", "mililiters","g","gs","gram","grams","grs","gr"],["t","ts","ton","tons"]]
+wt_get = [1,0.001,1000]
+nt_units = [["g","gs","gr","grs","gram","grams"],["mg","mgs","miligram","miligrams"],["kg","kgs","kilo","kilos","kilogram","kilograms"],["t","ts","ton","tons"]]
+nt_get=[1000,1,1000000,1000000000]
